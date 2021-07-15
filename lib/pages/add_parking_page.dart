ddece9d6dbc6e6d681a0bedda658b9ad3dedea7a
Kamil Matula <43967269+MKamil99@users.noreply.github.com>
2021-07-15 17:11:08 +0200

    Added basic view in Add Parking Page
    Added some Text Fields in Add Parking Page so user can insert longitude, latitude, name, description and ranking. The data is not added to database yet.

--- lib/pages/add_parking_page.dart
+++ lib/pages/add_parking_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class AddParkingPage extends StatefulWidget {
   const AddParkingPage({Key? key}) : super(key: key);
@@ -8,12 +9,84 @@ class AddParkingPage extends StatefulWidget {
 }
 
 class _AddParkingPageState extends State<AddParkingPage> with AutomaticKeepAliveClientMixin {
+  // Data:
+  double? latitude;
+  double? longitude;
+  String? name;
+  String? description;
+  int? ranking;
+  bool canAdd() => latitude != null && longitude != null && name != null && name!.isNotEmpty;
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
     return Scaffold(
-      body: Center(child: Text('Add Parking Page')),
+      body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // Just integers (for now):
+                TextField(
+                  decoration: InputDecoration(labelText: "Latitude"),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
+                  onChanged: (str) {
+                    setState(() {
+                      latitude = str.length > 0 ? double.parse(str) : null;
+                    });
+                  },
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: "Longitude"),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
+                  onChanged: (str) {
+                    setState(() {
+                      longitude = str.length > 0 ? double.parse(str) : null;
+                    });
+                  },
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: "Name"),
+                  onChanged: (str) { setState(() { name = str; }); },
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: "Description"),
+                  onChanged: (str) { setState(() { description = str; }); },
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: "Ranking"),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
+                  onChanged: (str) {
+                    setState(() {
+                      ranking  = str.length > 0 ? int.parse(str) : null;
+                    });
+                  },
+                ),
+                ElevatedButton(
+                  // Null makes the button disable:
+                  onPressed: canAdd() ? () => addLocation() : null,
+                  child: Text("Add"),
+                ),
+              ],
+            ),
+          )
+      ),
     );
+  }
+
+  // Adding parking location to database:
+  void addLocation() {
+
   }
 
   // Making sure that state will not be lost after changing page:
