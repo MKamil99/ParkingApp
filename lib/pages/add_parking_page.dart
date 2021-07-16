bc2d5bdb99a638c3b341755f46f05d2c47e0da07
Kamil Matula <43967269+MKamil99@users.noreply.github.com>
2021-07-16 22:50:40 +0200

    Improved Add Parking Page
    Added mini-map for choosing location and Ranking Bar for rating parking place by stars. Added "TODO" comments.

--- lib/pages/add_parking_page.dart
+++ lib/pages/add_parking_page.dart
@@ -1,5 +1,13 @@
+// Minor part of the code used in this file bases on
+// https://stackoverflow.com/questions/53652573/fix-google-map-marker-in-center
+
+// TODO: Fix layout (keyboard overflows text fields), clear fields after submitting,
+// TODO: locate user on map (position + zoom; to improve adding new parking),
+// TODO: change pin icon, add "Rating" label
+
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:parking_app/db/moor_database.dart';
 
@@ -12,78 +20,80 @@ class AddParkingPage extends StatefulWidget {
 
 class _AddParkingPageState extends State<AddParkingPage> with AutomaticKeepAliveClientMixin {
   // Data:
-  double? latitude;
-  double? longitude;
-  String name = '';
-  String description = '';
-  int? ranking;
-  bool canAdd() => latitude != null && longitude != null && name.isNotEmpty;
+  double? _latitude;
+  double? _longitude;
+  String _name = '';
+  String _description = '';
+  int? _ranking;
+  bool canAdd() => _latitude != null && _longitude != null && _name.isNotEmpty;
+
+  static final CameraPosition _initialPosition = CameraPosition(
+    target: LatLng(50.36, 18.93),
+    zoom: 9,
+  );
 
   @override
   Widget build(BuildContext context) {
     super.build(context);
     final database = Provider.of<AppDatabase>(context);
+    double mapHeight = MediaQuery.of(context).size.height / 3;
+    double mapWidth = MediaQuery.of(context).size.width;
+    double iconSize = 30.0;
 
     return Scaffold(
-      body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                // Just integers (for now):
-                TextField(
-                  decoration: InputDecoration(labelText: "Latitude"),
-                  keyboardType: TextInputType.number,
-                  inputFormatters: <TextInputFormatter>[
-                    FilteringTextInputFormatter.digitsOnly
-                  ],
-                  onChanged: (str) {
-                    setState(() {
-                      latitude = str.length > 0 ? double.parse(str) : null;
-                    });
-                  },
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                height: mapHeight,
+                width: mapWidth,
+                child: GoogleMap(
+                  initialCameraPosition: _initialPosition,
+                  onCameraMove: ((position) => updatePosition(position)),
                 ),
-                TextField(
-                  decoration: InputDecoration(labelText: "Longitude"),
-                  keyboardType: TextInputType.number,
-                  inputFormatters: <TextInputFormatter>[
-                    FilteringTextInputFormatter.digitsOnly
-                  ],
-                  onChanged: (str) {
-                    setState(() {
-                      longitude = str.length > 0 ? double.parse(str) : null;
-                    });
-                  },
-                ),
-                TextField(
-                  decoration: InputDecoration(labelText: "Name"),
-                  onChanged: (str) { setState(() { name = str; }); },
-                ),
-                TextField(
-                  decoration: InputDecoration(labelText: "Description"),
-                  onChanged: (str) { setState(() { description = str; }); },
-                ),
-                TextField(
-                  decoration: InputDecoration(labelText: "Ranking"),
-                  keyboardType: TextInputType.number,
-                  inputFormatters: <TextInputFormatter>[
-                    FilteringTextInputFormatter.digitsOnly
-                  ],
-                  onChanged: (str) {
-                    setState(() {
-                      ranking  = str.length > 0 ? int.parse(str) : null;
-                    });
-                  },
-                ),
-                ElevatedButton(
-                  // Null makes the button disable:
-                  onPressed: canAdd() ? () => addLocation(context, database) : null,
-                  child: Text("Add"),
-                ),
-              ],
+              ),
+              Positioned(
+                top: (mapHeight - iconSize) / 2,
+                left: (mapWidth - iconSize) / 2,
+                child: Icon(Icons.push_pin, color: Colors.red.shade800, size: iconSize),
+              ),
+            ],
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TextField(
+                    decoration: InputDecoration(labelText: "Name"),
+                    onChanged: (str) { setState(() { _name = str; }); },
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: "Description"),
+                    onChanged: (str) { setState(() { _description = str; }); },
+                  ),
+                  RatingBar.builder(
+                    itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                    itemBuilder: (context, _) => Icon(
+                      Icons.star,
+                      color: Colors.amber,
+                    ),
+                    onRatingUpdate: (rating) {
+                      _ranking = rating.toInt();
+                    },
+                  ),
+                ],
+              ),
             ),
-          )
+          ),
+          ElevatedButton(
+            // Null makes the button disable:
+            onPressed: canAdd() ? () => addLocation(context, database) : null,
+            child: Text("Add"),
+          ),
+        ],
       ),
     );
   }
@@ -93,14 +103,20 @@ class _AddParkingPageState extends State<AddParkingPage> with AutomaticKeepAlive
     setState(() {
       database.insertLocation(
           Location(
-              name: name,
-              latitude: latitude!,
-              longitude: longitude!,
-              description: description,
-              ranking: ranking
+              name: _name,
+              latitude: _latitude!,
+              longitude: _longitude!,
+              description: _description,
+              ranking: _ranking
           )
       );
     });
+  }
+
+  // Updating _latitude and _longitude variables after moving camera:
+  void updatePosition(CameraPosition position) {
+    _longitude = position.target.longitude;
+    _latitude = position.target.latitude;
   }
 
   // Making sure that state will not be lost after changing page:
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
-import 'dart:async';
+// TODO: Locate user on map (+ button), show last-added location at the start,
+// TODO: display ranking, add possibility to delete (or even edit) location
 
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
